13a8ed4a6a4cbec7b707581e3fe8a20320ed13c1
BrianRicci <yunus.ibragimov49@gmail.com>
2024-04-27 21:26:07 +0300

    Подготовка к внедрению api

--- config/settings.py
+++ config/settings.py
@@ -41,9 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libs
+    'rest_framework',
     # apps
     'cafe.apps.CafeConfig',
     'menu.apps.MenuConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
@@ -84,8 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kh_test',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
